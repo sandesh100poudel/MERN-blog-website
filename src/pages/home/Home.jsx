50dfae04dbbbf2c6a5e092742de871022d90271f
sandesh poudel <sandesh100poudel@gmail.com>
2022-09-13 14:27:11 +0545

    creating header component using home

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import  "./home.css";
+import Header from '../../header/Header';
+
+export default function Home() {
+  return (
+    <div className="home">
+        <Header />
+        Homepage</div>
+  )
+}
